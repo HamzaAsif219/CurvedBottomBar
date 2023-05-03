43539becb6c7464749f5fbebf0dae6a35d6639e7
HamzaAsif219 <91444044+HamzaAsif219@users.noreply.github.com>
2023-05-03 16:40:46 +0500

    Delete Example.js

--- Example.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SwitchToggle from "react-native-switch-toggle";
-function example(props) {
-    return (
-        <SwitchToggle switchOn={on} onPress={() => off(!on)} />
-    );
-}
-
-export default example;
